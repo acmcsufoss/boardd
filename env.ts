27bd51dc1b21b623209aa32ccb92a1aac6229098
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-05-20 04:12:24 -0700

    Revert "Allow empty environment variable values for prod"
    This reverts commit 283661ceef793e7ccf8c860b6b328af090dd36fe.

--- env.ts
+++ env.ts
@@ -1,49 +1,45 @@
 import { dotenv } from "./deps.ts";
 
-await dotenv.load({ export: true });
+const env = await dotenv.load();
 
 /**
  * PORT is the port to listen on.
  */
-export const PORT = parseInt(Deno.env.get("PORT") || "8080");
+export const PORT = parseInt(env.PORT || "8080");
 
 /**
  * DEV is true if the application is running in development mode.
  */
-export const DEV = Deno.env.get("DEV") === "true";
+export const DEV = env.DEV === "true";
 
-const RAW_DISCORD_PUBLIC_KEY = Deno.env.get("DISCORD_PUBLIC_KEY");
-if (!RAW_DISCORD_PUBLIC_KEY) {
+if (!env.DISCORD_PUBLIC_KEY) {
   throw new Error("DISCORD_PUBLIC_KEY environment variable is required");
 }
 
 /**
  * DISCORD_PUBLIC_KEY is the Discord bot public key.
  */
-export const DISCORD_PUBLIC_KEY = RAW_DISCORD_PUBLIC_KEY;
+export const DISCORD_PUBLIC_KEY = env.DISCORD_PUBLIC_KEY;
 
-const RAW_DISCORD_CLIENT_ID = Deno.env.get("DISCORD_CLIENT_ID");
-if (!RAW_DISCORD_CLIENT_ID) {
+if (!env.DISCORD_CLIENT_ID) {
   throw new Error("DISCORD_CLIENT_ID environment variable is required");
 }
 
 /**
  * DISCORD_CLIENT_ID is the Discord bot client ID.
  */
-export const DISCORD_CLIENT_ID = RAW_DISCORD_CLIENT_ID;
+export const DISCORD_CLIENT_ID = env.DISCORD_CLIENT_ID;
 
-const RAW_DISCORD_TOKEN = Deno.env.get("DISCORD_TOKEN");
-if (!RAW_DISCORD_TOKEN) {
+if (!env.DISCORD_TOKEN) {
   throw new Error("DISCORD_TOKEN environment variable is required");
 }
 
 /**
  * DISCORD_TOKEN is the Discord bot token.
  */
-export const DISCORD_TOKEN = RAW_DISCORD_TOKEN;
+export const DISCORD_TOKEN = env.DISCORD_TOKEN;
 
-const RAW_DISCORD_ROLE_ID = Deno.env.get("DISCORD_ROLE_ID");
-if (!RAW_DISCORD_ROLE_ID) {
+if (!env.DISCORD_ROLE_ID) {
   throw new Error("DISCORD_ROLE_ID environment variable is required");
 }
 
@@ -51,10 +47,9 @@ if (!RAW_DISCORD_ROLE_ID) {
  * DISCORD_ROLE_ID is the Discord role ID that is allowed to use the Boardd
  * application command. The command is intended to be used by board members.
  */
-export const DISCORD_ROLE_ID = RAW_DISCORD_ROLE_ID;
+export const DISCORD_ROLE_ID = env.DISCORD_ROLE_ID;
 
-const RAW_DISCORD_ADMIN_ROLES = Deno.env.get("DISCORD_ADMIN_ROLES");
-if (!RAW_DISCORD_ADMIN_ROLES) {
+if (!env.DISCORD_ADMIN_ROLES) {
   throw new Error("DISCORD_ADMIN_ROLES environment variable is required");
 }
 
@@ -62,14 +57,13 @@ if (!RAW_DISCORD_ADMIN_ROLES) {
  * DISCORD_ADMIN_ROLES is a comma-separated list of Discord role IDs that are
  * allowed to use the Boardd application command on any user.
  */
-export const DISCORD_ADMIN_ROLES = RAW_DISCORD_ADMIN_ROLES.split(",");
+export const DISCORD_ADMIN_ROLES = env.DISCORD_ADMIN_ROLES.split(",");
 
-const RAW_GITHUB_TOKEN = Deno.env.get("GITHUB_TOKEN");
-if (!RAW_GITHUB_TOKEN) {
+if (!env.GITHUB_TOKEN) {
   throw new Error("GITHUB_TOKEN environment variable is required");
 }
 
 /**
  * GITHUB_TOKEN is the GitHub personal access token.
  */
-export const GITHUB_TOKEN = RAW_GITHUB_TOKEN;
+export const GITHUB_TOKEN = env.GITHUB_TOKEN;
